eb1061d5a78bece5934158b21f6d7b2480d3364a
Joseph Camarena <pepecamarena5@gmail.com>
2024-03-30 13:17:48 -0700

    fixed type error for "any" type within Logger.ts

--- front-end/src/utility/Logger.ts
+++ front-end/src/utility/Logger.ts
@@ -1,23 +1,25 @@
 type LogLevel = 'debug' | 'info' | 'warn' | 'error';
 
+type AdditionalData = unknown[];
+
 class Logger {
-  static log(level: LogLevel, message: string, ...additional: any[]) {
+  static log(level: LogLevel, message: string, ...additional: AdditionalData) {
     console[level](`[${new Date().toISOString()}] ${message}`, ...additional);
   }
 
-  static debug(message: string, ...additional: any[]) {
+  static debug(message: string, ...additional: AdditionalData) {
     this.log('debug', message, ...additional);
   }
 
-  static info(message: string, ...additional: any[]) {
+  static info(message: string, ...additional: AdditionalData) {
     this.log('info', message, ...additional);
   }
 
-  static warn(message: string, ...additional: any[]) {
+  static warn(message: string, ...additional: AdditionalData) {
     this.log('warn', message, ...additional);
   }
 
-  static error(message: string, ...additional: any[]) {
+  static error(message: string, ...additional: AdditionalData) {
     this.log('error', message, ...additional);
   }
 }
