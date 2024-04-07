05ff0fcd08f54230a41e76de37294e9e54649e83
Joseph Camarena <pepecamarena5@gmail.com>
2024-04-07 12:29:50 -0700

    testing

--- front-end/src/components/Login.tsx
+++ front-end/src/components/Login.tsx
@@ -6,7 +6,7 @@ const Login:React.FC<LoginProps> = ({ onLogin }) => {
   return (
     <div className="jumbotron text-center">
       <h1 className="display-4">Welcome</h1>
-      <p className="lead">Please log in to manage customers.</p>
+      <p className="lead">Im Testing</p>
       <button className="btn btn-primary btn-lg" onClick={onLogin}>Login</button>
     </div>
   );
